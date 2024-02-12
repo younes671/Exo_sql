4df3e6f22d0f2063c37f2425f67b447b3c01e573
Younesse AL AMRI <younes671@live.fr>
2024-02-12 13:34:06 +0100

    commit modification detail.potion.php

--- Potion.php
+++ Potion.php
@@ -5,7 +5,7 @@ require_once "MonPDO.php";
 
 
 if($monPDO){
-    $req = 'SELECT p.id_potion AS "Numéro", p.nom_potion AS "Nom_potion", SUM(c.qte * i.cout_ingredient) AS "Cout potion"
+    $req = 'SELECT p.id_potion AS "Numero", p.nom_potion AS "Nom_potion", SUM(c.qte * i.cout_ingredient) AS "Cout potion"
     FROM potion p
     INNER JOIN composer c ON c.id_potion = p.id_potion
     INNER JOIN ingredient i ON i.id_ingredient = c.id_ingredient
@@ -27,8 +27,8 @@ if($monPDO){
     foreach($potions as $potion){
          ?>
           <tr>
-          <td><?=$potion['Numéro']?></td>
-          <td><a href="detail.potion.php?Numéro=<?=$potion['Numéro']?>&Nom_potion=<?=$potion['Nom_potion']?>" class="badge badge-light text-dark text-decoration-none" ><?=$potion['Nom_potion']?></a></td>
+          <td><?=$potion['Numero']?></td>
+          <td><a href="detail.potion.php?Numero=<?=$potion['Numero']?>&Nom_potion=<?=$potion['Nom_potion']?>" class="badge badge-light text-dark text-decoration-none" ><?=$potion['Nom_potion']?></a></td>
           <td><?=$potion['Cout potion']?></td>
           </tr>
       <?php
--- detail.potion.php
+++ detail.potion.php
@@ -8,36 +8,34 @@ if($monPDO){
     $req = 'SELECT p.id_potion AS "id", p.nom_potion AS "nom", i.nom_ingredient AS "nom_ingredient", i.cout_ingredient AS "cout", c.qte AS "quantite"
     FROM potion p
     INNER JOIN composer c ON c.id_potion = p.id_potion
-    INNER JOIN ingredient i ON i.id_ingredient = c.id_ingredient   
+    INNER JOIN ingredient i ON i.id_ingredient = c.id_ingredient 
+    WHERE p.id_potion = :id  
     ';
     $stmt =$monPDO->prepare($req);
-    $stmt->execute();
+    $id = $_GET["Numero"];
+    $stmt->execute(["id"=>$id]);
     $detailpotions = $stmt->fetchAll();
-    ?><table class="table table-striped">
+?>  <table class="table table-striped">
         <thead>
           <tr>
-            <th scope="col">Nom Potion</th>
             <th scope="col">Ingrédient</th>
             <th scope="col">Cout ingrédient</th>
             <th scope="col">Quantité</th>
           </tr>
         </thead>
         <tbody>
-            <?php
-    foreach($detailpotions as $detailpotion){
-         
-         if($detailpotion['id'] == $_GET['Numéro']){
-    ?>       <tr>
-             <td><?=$detailpotion['nom_ingredient']?></td>
-             <td><?=$detailpotion['cout']?></td>
-             <td><?=$detailpotion['quantite']?></td>
-             </tr>
-    <?php
-            }
-    
-            }
-            ?> </tbody>
-            </table><?php
+        <?php    
+ foreach($detailpotions as $detailpotion){
+?>            <tr>
+                <td><?= $detailpotion['nom_ingredient'] ?></td>
+                <td><?= $detailpotion['cout'] ?></td>
+                <td><?= $detailpotion['quantite'] ?></td>
+              </tr>
+<?php
+             }
+?> 
+        </tbody>
+    </table><?php
     
 } 
 
